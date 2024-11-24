9b57e0ec642ef4c8a069e627fd930408683ec61f
Patrick Muorah <pm00058@my.westga.edu>
2024-11-24 22:52:28 +0100

    Created a HighScore class to hold data for players' scores

--- PatrickMuorahProject2/Model/HighScore.cs
+++ PatrickMuorahProject2/Model/HighScore.cs
@@ -8,6 +8,20 @@ namespace PatrickMuorahProject2.Model
 {
     internal class HighScore
     {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public string Time { get; set; } // Format: "MM:SS"
 
+        public HighScore(string name, int score, string time)
+        {
+            Name = name;
+            Score = score;
+            Time = time;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Score}, {Time}";
+        }
     }
 }
