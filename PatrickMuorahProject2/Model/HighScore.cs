446b4644073620673c34a7dc5616618e2810ba23
Patrick Muorah <pm00058@my.westga.edu>
2024-11-25 02:06:53 +0100

    Added documentaions for the HighScores class

--- PatrickMuorahProject2/Model/HighScore.cs
+++ PatrickMuorahProject2/Model/HighScore.cs
@@ -6,12 +6,32 @@ using System.Threading.Tasks;
 
 namespace PatrickMuorahProject2.Model
 {
+    /// <summary>
+    /// Tracks the data for the high scores in the game.
+    /// </summary>
     internal class HighScore
     {
+        /// <summary>
+        /// Gets or sets the name of the player who got the high score.
+        /// </summary>
         public string Name { get; set; }
-        public int Score { get; set; }
-        public string Time { get; set; } // Format: "MM:SS"
 
+        /// <summary>
+        /// Gets or sets the high score obtained by thye player.
+        /// </summary>
+        public int Score { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time when the game was completed.
+        /// </summary>
+        public string Time { get; set; }
+
+        /// <summary>
+        /// Constructor method
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="score"></param>
+        /// <param name="time"></param>
         public HighScore(string name, int score, string time)
         {
             Name = name;
@@ -19,6 +39,10 @@ namespace PatrickMuorahProject2.Model
             Time = time;
         }
 
+        /// <summary>
+        /// Formats the high score as a string.
+        /// </summary>
+        /// <returns>A string that represents the high score.</returns>
         public override string ToString()
         {
             return $"{Name}, {Score}, {Time}";
