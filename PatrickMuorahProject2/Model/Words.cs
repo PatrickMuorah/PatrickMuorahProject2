1dc10ac34eef93d35de53546d1b8a2bb9bf6e2e0
Patrick Muorah <pm00058@my.westga.edu>
2024-11-25 01:59:51 +0100

    Added documentations to the Words class

--- PatrickMuorahProject2/Model/Words.cs
+++ PatrickMuorahProject2/Model/Words.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace PatrickMuorahProject2.Model
 {
+    /// <summary>
+    /// This class is used to track valid and invalid words entered by a player.
+    /// </summary>
     internal class Words
     {
+        /// <summary>
+        /// Gets or sets a word entered by the player.
+        /// </summary>
         public string? Word { get; set; }
+
+        /// <summary>
+        /// Gets or sets the points earned for a word.
+        /// </summary>
         public int Points { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time when a word was entered by the player.
+        /// </summary>
         public DateTime TimeEntered { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reason a word was invalid.
+        /// </summary>
         public string? Reason { get; set; }
 
     }
